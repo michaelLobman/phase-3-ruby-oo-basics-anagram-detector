311865ade2e2b7c29c876e7e5bd153247e39f091
Michael Lobman <michael.lobman@gmail.com>
2022-01-12 13:22:45 -0500

    Complete assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_reader :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        chars_to_match = word.chars.sort
+        array.filter do |case_word|
+            case_word.chars.sort == chars_to_match
+        end
+    end
+
+end
